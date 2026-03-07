955e9321ddcf01b58346e91e80a13baaef59aba4
Artur Borowy <artur.borowy843@gmail.com>
2026-03-07 08:49:55 +0100

    Create CoroutineExceptionHandlerTest.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
 }
@@ -30,8 +32,14 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_21
     }
 }
 
@@ -39,7 +47,12 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+
     testImplementation(libs.junit)
+    testImplementation(libs.kotlinx.coroutines.core)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.mockk)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
---
+++ app/src/test/java/com/arturborowy/androidtests/coroutines/CoroutineExceptionHandlerTest.kt
@@ -0,0 +1,149 @@
+package com.arturborowy.androidtests.coroutines
+
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.runs
+import io.mockk.verify
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertThrows
+import org.junit.Before
+import org.junit.Test
+
+class CoroutineExceptionHandlerTest {
+
+    private interface ThrowableHandlingDelegate {
+        fun handle(throwable: Throwable)
+    }
+
+    private val throwableHandlingDelegate = mockk<ThrowableHandlingDelegate>()
+
+    private val coroutineExceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        throwableHandlingDelegate.handle(throwable)
+    }
+
+    private val scope = CoroutineScope(coroutineExceptionHandler)
+
+    private val exceptionToThrow = RuntimeException()
+
+    @Before
+    fun setUp() {
+        every { throwableHandlingDelegate.handle(exceptionToThrow) } just runs
+    }
+
+    @Test
+    fun `CoroutineExceptionHandler is called when exception is thrown from root coroutine launch`() {
+        val job = scope.launch {
+            throw exceptionToThrow
+        }
+
+        runBlocking { job.join() }
+
+        verify { throwableHandlingDelegate.handle(exceptionToThrow) }
+    }
+
+    @Test
+    fun `CoroutineExceptionHandler is called with original exception (not wrapped in CancellationException) when is thrown from child coroutine launch`() {
+        val job = scope.launch {
+            launch {
+                throw exceptionToThrow
+            }
+        }
+
+        runBlocking { job.join() }
+
+        verify { throwableHandlingDelegate.handle(exceptionToThrow) }
+    }
+
+    @Test
+    fun `CoroutineExceptionHandler does not recover coroutine`() {
+        var executedAfterException = false
+
+        val job = scope.launch {
+            throw exceptionToThrow
+            @Suppress("UNREACHABLE_CODE")
+            executedAfterException = true
+
+        }
+
+        runBlocking { job.join() }
+
+        assertFalse(job.isActive)
+        assertFalse(executedAfterException)
+    }
+
+    @Test
+    fun `expected exception is thrown when CoroutineExceptionHandler is passed to child coroutine of runTest`() {
+        val exceptionThrown = assertThrows(RuntimeException::class.java) {
+            runTest {
+                val job = launch(coroutineExceptionHandler) {
+                    throw exceptionToThrow
+                }
+
+                job.join()
+            }
+        }
+
+        assertEquals(exceptionToThrow, exceptionThrown)
+
+        verify(inverse = true) { throwableHandlingDelegate.handle(exceptionToThrow) }
+    }
+
+    @Test
+    fun `CoroutineExceptionHandler silently ignores CancellationException`() {
+        val cancellationExceptionToThrow = CancellationException()
+        val job = scope.launch {
+            cancel(cancellationExceptionToThrow)
+            throw exceptionToThrow
+        }
+
+        runBlocking { job.join() }
+
+        verify { throwableHandlingDelegate.handle(exceptionToThrow) }
+        verify(inverse = true) { throwableHandlingDelegate.handle(cancellationExceptionToThrow) }
+    }
+
+    @Test
+    fun `CoroutineExceptionHandler is NOT called when exception is thrown from root coroutine async`() {
+        val deferred = scope.async(coroutineExceptionHandler) {
+            throw exceptionToThrow
+        }
+
+        val exceptionThrown = assertThrows(RuntimeException::class.java) {
+            runBlocking { deferred.await() }
+        }
+
+        assertEquals(RuntimeException::class.java, exceptionThrown.javaClass)
+        verify(inverse = true) { throwableHandlingDelegate.handle(any()) }
+
+    }
+
+    @Test
+    fun `CoroutineExceptionHandler is NOT called when the exception is catch`() {
+        val tryCatchThrowableHandlingDelegate = mockk<ThrowableHandlingDelegate>()
+
+        every { tryCatchThrowableHandlingDelegate.handle(exceptionToThrow) } just runs
+
+        val job = scope.launch {
+            try {
+                throw exceptionToThrow
+            } catch (runtimeException: RuntimeException) {
+                tryCatchThrowableHandlingDelegate.handle(runtimeException)
+            }
+        }
+
+        runBlocking { job.join() }
+
+        verify(inverse = true) { throwableHandlingDelegate.handle(exceptionToThrow) }
+        verify { tryCatchThrowableHandlingDelegate.handle(exceptionToThrow) }
+    }
+}
