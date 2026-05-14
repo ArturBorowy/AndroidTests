ae7c4cc1141834923e7787cc44fb0dbaa066d537
Artur Borowy <artur.borowy843@gmail.com>
2026-05-14 14:23:35 +0200

    AnimatedVisibilityExample

---
+++ app/src/main/java/com/arturborowy/androidtests/ContentExample.kt
@@ -0,0 +1,37 @@
+package com.arturborowy.androidtests
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+fun ContentExample() {
+    Column {
+        Text(
+            "Header",
+            Modifier
+                .background(Color.Cyan, RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp))
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+        Text(
+            "Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body",
+            Modifier
+                .background(
+                    Color.White,
+                    RoundedCornerShape(bottomStart = 16.dp, bottomEnd = 16.dp)
+                )
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+    }
+}
--- app/src/main/java/com/arturborowy/androidtests/MainActivity.kt
+++ app/src/main/java/com/arturborowy/androidtests/MainActivity.kt
@@ -4,32 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Icon
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
-import com.arturborowy.androidtests.ui.theme.AndroidTestsTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            AndroidTestsTheme {
-                AndroidTestsApp()
-            }
+            AndroidTestsApp()
         }
     }
 }
@@ -37,55 +36,28 @@ class MainActivity : ComponentActivity() {
 @PreviewScreenSizes
 @Composable
 fun AndroidTestsApp() {
-    var currentDestination by rememberSaveable { mutableStateOf(AppDestinations.HOME) }
-
-    NavigationSuiteScaffold(
-        navigationSuiteItems = {
-            AppDestinations.entries.forEach {
-                item(
-                    icon = {
-                        Icon(
-                            painterResource(it.icon),
-                            contentDescription = it.label
-                        )
-                    },
-                    label = { Text(it.label) },
-                    selected = it == currentDestination,
-                    onClick = { currentDestination = it }
-                )
-            }
-        }
-    ) {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            Greeting(
-                name = "Android",
-                modifier = Modifier.padding(innerPadding)
-            )
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(16.dp)
+        ) {
+            AnimatedVisibilityExample()
         }
     }
 }
 
-enum class AppDestinations(
-    val label: String,
-    val icon: Int,
-) {
-    HOME("Home", R.drawable.ic_home),
-    FAVORITES("Favorites", R.drawable.ic_favorite),
-    PROFILE("Profile", R.drawable.ic_account_box),
-}
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+private fun AnimatedVisibilityExample() {
+    var isVisible by remember { mutableStateOf(true) }
+    AnimatedVisibility(isVisible) {
+        ContentExample()
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidTestsTheme {
-        Greeting("Android")
+    Spacer(Modifier.height(16.dp))
+
+    Button(onClick = { isVisible = isVisible.not() }) {
+        Text("AnimatedVisibilityExample")
     }
 }
