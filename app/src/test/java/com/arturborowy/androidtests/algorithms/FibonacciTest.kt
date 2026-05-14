1338cdc8bc33166658140ad526dda579e8fcf526
Artur Borowy <artur.borowy843@gmail.com>
2026-05-14 14:05:44 +0200

    Generate new navigation activity

--- app/src/test/java/com/arturborowy/androidtests/algorithms/FibonacciTest.kt
+++ app/src/test/java/com/arturborowy/androidtests/algorithms/FibonacciTest.kt
@@ -3,6 +3,7 @@ package com.arturborowy.androidtests.algorithms
 import junit.framework.TestCase.assertEquals
 import org.junit.Test
 
+// TODO add performance tests
 class FibonacciTest {
 
     private val expectedFibonacciSequence = intArrayOf(0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144)
